55e667f32a585e9c94d48f27b44c4a86144a0d7b
Pack Craftsmanship <pack_soi4@hotmail.com>
2025-02-17 12:10:19 +0700

    update

--- src/components/navigation/nav-items.tsx
+++ src/components/navigation/nav-items.tsx
@@ -5,7 +5,7 @@ const NavItem = (props: { href: string; name: string }) => {
 	return (
 		<Link
 			href={props.href}
-			className='text-white uppercase pr-4 md:border-r md:last:border-r-0'
+			className='text-white uppercase pr-4 xl:border-r xl:last:border-r-0'
 		>
 			{props.name}
 		</Link>
